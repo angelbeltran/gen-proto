f0c4bdebb11f6613666d2ecce0207eab3ff2a9f9
Angel Beltran <beltranbeaverdam@gmail.com>
2016-10-27 09:28:22 -0600

    nested message types; moved logic to functions

--- index.js
+++ index.js
@@ -10,59 +10,23 @@ module.exports = function generateProtobuf (protoDef, cb) {
   }
 
   // syntax statement
-  switch (protoDef.syntax) {
-    case (2):
-      file.push('syntax = "proto2";')
-      break
-    case (3):
-    case (undefined):
-      file.push('syntax = "proto3";')
-      break
-    default:
-      throw new Error('Syntax must be 2 or 3')
-  }
+  writeSyntax(protoDef.syntax, file)
 
   // package name
-  if (!protoDef.package) {
-    throw new Error('Package name required')
-  }
-
-  file.push('package ' + protoDef.package + ';')
+  writePackageName(protoDef.package, file)
   file.push('')
 
   // option statements
-  if (protoDef.options) {
-    if (Object.keys(protoDef.options).length) {
-      Object.keys(protoDef.options).forEach((opt) => {
-        file.push('option ' + opt + ' = "' + protoDef.options[opt] + '";')
-      })
-      file.push('')
-    }
-  }
+  writeOptions(protoDef.options, file)
 
   // import statements
-  if (protoDef.imports) {
-    protoDef.imports.forEach((imp) => {
-      file.push('import ' + (imp.public ? 'public ' : '') + '"' + imp.filename + '";')
-    })
-    file.push('')
-  }
+  writeImports(protoDef.imports, file)
 
   // messages
+  // TODO: handle proto2 required and optional keywords
   if (protoDef.messages) {
-    protoDef.messages.forEach((msg) => {
-      if (msg.description) {
-        file.push('// ' + msg.description)
-      }
-      file.push('message ' + msg.name + ' {')
-      if (msg.schema) {
-        msg.schema.forEach((field, i) => {
-          file.push('  ' + (field.repeated ? 'repeated ' : '') + field.type + ' ' + field.name + ' = ' + (field.key ? field.key : (i + 1)) + ';')
-        })
-      }
-      file.push('}')
-      file.push('')
-    })
+    writeMessages(protoDef.messages, file)
+    file.push('')
   }
 
   // service
@@ -85,4 +49,92 @@ module.exports = function generateProtobuf (protoDef, cb) {
   } else {
     require('fs').writeFileSync(protoDef.filename, file.join('\n'))
   }
+}
+
+/* Writes the syntax of a proto def to the file string array
+ *
+ */
+function writeSyntax (syntax, file) {
+  switch (syntax) {
+    case (2):
+      file.push('syntax = "proto2";')
+      break
+    case (3):
+    case (undefined):
+      file.push('syntax = "proto3";')
+      break
+    default:
+      throw new Error('Syntax must be 2 or 3')
+  }
+}
+
+/* Writes the package name of the proto def to the file string array
+ *
+ */
+function writePackageName (packageName, file) {
+  if (!packageName) {
+    throw new Error('Package name required')
+  }
+
+  file.push('package ' + packageName + ';')
+}
+
+/* Writes the options of the proto def to the file string array
+ *
+ */
+function writeOptions (options, file) {
+  if (options) {
+    if (Object.keys(options).length) {
+      Object.keys(options).forEach((opt) => {
+        file.push('option ' + opt + ' = "' + options[opt] + '";')
+      })
+      file.push('')
+    }
+  }
+}
+
+/* Writes the imports of the proto def to the file string array
+ *
+ */
+function writeImports (imports, file) {
+  if (imports) {
+    imports.forEach((imp) => {
+      file.push('import ' + (imp.public ? 'public ' : '') + '"' + imp.filename + '";')
+    })
+    file.push('')
+  }
+}
+
+/* Writes the message of a proto def to the file string array
+ * 
+ */
+function writeMessages (messages, file, level) {
+  level = level || 0
+  let padding = ''
+
+  for (let i = 0; i < level; i++) {
+    padding += '  '
+  }
+
+  messages.forEach((msg) => {
+    if (msg.description) {
+      file.push(padding + '// ' + msg.description)
+    }
+    file.push(padding + 'message ' + msg.name + ' {')
+    if (msg.schema) {
+      msg.schema.forEach((field, i) => {
+        file.push(padding + '  ' + (field.repeated ? 'repeated ' : '') + field.type + ' ' + field.name + ' = ' + (field.key ? field.key : (i + 1)) + ';')
+      })
+    }
+
+    if (msg.messages) {
+      file.push('')
+      writeMessages(msg.messages, file, level + 1)
+    }
+
+    file.push(padding + '}')
+    file.push(padding)
+  })
+
+  file.pop()
 }
